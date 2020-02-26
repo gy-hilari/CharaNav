eb8efa8f8e3bc104595a84374553ca3f0734fdb9
Gabe H <gy.hilari@gmail.com>
2020-02-26 14:25:33 -0800

    Began work on stylization

--- src/components/compendiumList/compendiumList.js
+++ src/components/compendiumList/compendiumList.js
@@ -3,30 +3,35 @@ import Aux from '../../hoc/Auxi';
 import './compendiumList.css'
 
 const CompendiumList = (props) => {
-    return props.comps ? 
-    (
-        <Aux>
-            {
-                props.comps.map((elm, idx) => {
-                    let num = idx + 1;
-                    return (
-                        <Aux key={elm.id}>
-                            <p className='Compendium' onClick={() => {
-                                props.getChars(elm.id);
-                                props.getArticles(elm.id);
-                                props.clearChar();
-                                props.getComp(elm.id);
-                            }}>{`${num}.) ${elm.name}`}</p>
-                            <button onClick={() => {
-                                props.delete(elm.id);
-                            }}>Delete</button>
-                        </Aux>
-                    );
-                })
-            }
-        </Aux>
-    ) 
-    :
-    null;
+    return props.comps ?
+        (
+            <Aux>
+                <div className="comp-scroller">
+                    <div className="buffer"></div>
+                {
+                    props.comps.map((elm, idx) => {
+                        // let num = idx + 1;
+                        return (
+                            <Aux key={elm.id}>
+                                    <div className="comp-card">
+                                        <h4 className='Compendium' onClick={() => {
+                                            props.getChars(elm.id);
+                                            props.getArticles(elm.id);
+                                            props.clearChar();
+                                            props.getComp(elm.id);
+                                        }}>{`${elm.name}`}</h4>
+                                        {/* <button onClick={() => {
+                                            props.delete(elm.id);
+                                        }}>Delete</button> */}
+                                    </div>
+                            </Aux>
+                        );
+                    })
+                }
+                </div>
+            </Aux>
+        )
+        :
+        null;
 };
 export default CompendiumList;
--- src/containers/compendiums/compendiums.js
+++ src/containers/compendiums/compendiums.js
@@ -67,7 +67,7 @@ class Compendiums extends Component {
             this.setState({ comps: res });
         });
     }
-    
+
     deleteCompById = (compId) => {
         window.api.promise('/delete/comp', compId, (res) => {
             console.log(res);
@@ -185,144 +185,143 @@ class Compendiums extends Component {
     }
 
     render() {
-        if (this.state.scene === 'comps') {
-            return (
-                <Aux>
-                    <button onClick={() => {console.log(new Date(Date.now()).toISOString())}}>Date Test</button>
-                    <input type="text" id="create-comp-name"/>
-                    <button onClick={() => this.createComp({ name: document.getElementById('create-comp-name').value })}>CREATE COMPENDIUM</button>
-                    <hr />
-                    <CompendiumList
-                        comps={this.state.comps}
-                        getComp={this.getComp}
-                        getChars={this.getCompChars}
-                        getArticles={this.getCompArticles}
-                        clearChar={this.clearActiveChar}
-                        delete={this.deleteCompById}
-                    />
-                </Aux>
-            );
-        }
-        if (this.state.scene === 'comp') {
-            return (
-                <Aux>
-                    <button onClick={() => {
-                        this.setState({ activeImageSelect: null });
-                        this.setState({ imgBrowse: false });
-                        this.setState({ scene: 'comps' });
-                    }}>Go Back</button>
-                    <hr />
-                    <ViewCompendium
-                        newChar={this.createChar}
-                        deleteChar={this.deleteCharById}
-                        newArticle={this.createArticle}
-                        deleteArticle={this.deleteArticleById}
-                        getLayers={this.getCharLayers}
-                        comp={this.state.activeComp}
-                        refresh={(id) => { 
-                            this.getComp(id);
-                            this.getComps();
-                        }}
-                        chars={this.state.compChars}
-                        getChar={this.getChar}
-                        getArticles={this.getCharArticles}
-                        setScene={this.setScene}
-                        imgDir={this.state.imgDir}
-                        setImgBrowse={this.setImageDirBrowser}
-                        imgBrowse={this.state.imgBrowse}
-                        activeImg={this.state.activeImageSelect}
-                        setActiveImg={this.setActiveImageSelect}
-                        refreshDir={this.getImageDir}
-                        formMode={this.state.formMode}
-                        setFormMode={this.setFormMode}
-                        setBrowseMode={this.setBrowseMode}
-                        browseMode={this.state.browseMode}
+        return (
+            <Aux>
+                <div className="main-scene">
+                    {
+                        this.state.scene === 'comps' &&
+                        <Aux>
+                            <input type="text" id="create-comp-name" />
+                            <button onClick={() => this.createComp({ name: document.getElementById('create-comp-name').value })}>CREATE COMPENDIUM</button>
+                            <CompendiumList
+                                comps={this.state.comps}
+                                getComp={this.getComp}
+                                getChars={this.getCompChars}
+                                getArticles={this.getCompArticles}
+                                clearChar={this.clearActiveChar}
+                                delete={this.deleteCompById}
+                            />
+                        </Aux>
+                    }
+                    {
+                        this.state.scene === 'comp' &&
+                        <Aux>
+                            <button onClick={() => {
+                                this.setState({ activeImageSelect: null });
+                                this.setState({ imgBrowse: false });
+                                this.setState({ scene: 'comps' });
+                            }}>Go Back</button>
+                            <hr />
+                            <ViewCompendium
+                                newChar={this.createChar}
+                                deleteChar={this.deleteCharById}
+                                newArticle={this.createArticle}
+                                deleteArticle={this.deleteArticleById}
+                                getLayers={this.getCharLayers}
+                                comp={this.state.activeComp}
+                                refresh={(id) => {
+                                    this.getComp(id);
+                                    this.getComps();
+                                }}
+                                chars={this.state.compChars}
+                                getChar={this.getChar}
+                                getArticles={this.getCharArticles}
+                                setScene={this.setScene}
+                                imgDir={this.state.imgDir}
+                                setImgBrowse={this.setImageDirBrowser}
+                                imgBrowse={this.state.imgBrowse}
+                                activeImg={this.state.activeImageSelect}
+                                setActiveImg={this.setActiveImageSelect}
+                                refreshDir={this.getImageDir}
+                                formMode={this.state.formMode}
+                                setFormMode={this.setFormMode}
+                                setBrowseMode={this.setBrowseMode}
+                                browseMode={this.state.browseMode}
 
-                        compArts={this.state.compArts}
-                        getArticle={this.getArticle}
-                        master={this.state.imgDir.master}
-                    />
-                </Aux>
-            );
-        }
-        if (this.state.scene === 'char') {
-            return (
-                <Aux>
-                    <div className="nav-ui">
-                        <button onClick={() => {
-                            this.clearActiveChar();
-                            this.setState({ scene: 'comp' });
-                        }}>Back to Compendium</button>
-                    </div>
-                    <LayerList
-                        articles={this.state.compArts}
-                        layers={this.state.charLayers}
-                        refreshAll={(id) => {
-                            this.getChar(id);
-                            this.getCompChars(this.state.activeComp.id);
-                        }}
-                        refresh={this.getCharLayers}
-                        newLayer={this.createLayer}
-                        char={this.state.activeChar}
-                        charId={this.state.activeChar.id}
-                        charArts={this.state.charArts}
-                        assign={this.assignArticleToChar}
-                        getArticles={this.getCharArticles}
-                        getArticle={this.getArticle}
-                        getChar={this.getChar}
-                        master={this.state.imgDir.master}
-                        delete={this.deleteLayerById}
-                    />
-                </Aux>
-            )
-        }
-        if (this.state.scene === 'article-browse') {
-            return (
-                <Aux>
-                    <button onClick={() => {
-                        this.clearActiveChar();
-                        this.setState({ scene: 'comp' });
-                    }}>Back to Compendium</button>
-                    {
-                        this.state.activeChar &&
-                        <button onClick={() => { this.setState({ scene: 'char' }); }}>
-                            Back to Character
-                        </button>
+                                compArts={this.state.compArts}
+                                getArticle={this.getArticle}
+                                master={this.state.imgDir.master}
+                            />
+                        </Aux>
                     }
-                    <hr />
-                    <ArticleBrowse
-                        articles={this.state.compArts}
-                        getArticle={this.getArticle}
-                        master={this.state.imgDir.master}
-                    />
-                </Aux>
-            )
-        }
-        if (this.state.scene === 'article') {
-            return (
-                <Aux>
-                    <button onClick={() => {
-                        this.clearActiveChar();
-                        this.setState({ scene: 'comp' });
-                    }}>Back to Compendium</button>
                     {
-                        this.state.activeChar &&
-                        <button onClick={() => { this.setState({ scene: 'char' }); }}>
-                            Back to Character
-                        </button>
+                        this.state.scene === 'char' &&
+                        <Aux>
+                            <div className="nav-ui">
+                                <button onClick={() => {
+                                    this.clearActiveChar();
+                                    this.setState({ scene: 'comp' });
+                                }}>Back to Compendium</button>
+                            </div>
+                            <LayerList
+                                articles={this.state.compArts}
+                                layers={this.state.charLayers}
+                                refreshAll={(id) => {
+                                    this.getChar(id);
+                                    this.getCompChars(this.state.activeComp.id);
+                                }}
+                                refresh={this.getCharLayers}
+                                newLayer={this.createLayer}
+                                char={this.state.activeChar}
+                                charId={this.state.activeChar.id}
+                                charArts={this.state.charArts}
+                                assign={this.assignArticleToChar}
+                                getArticles={this.getCharArticles}
+                                getArticle={this.getArticle}
+                                getChar={this.getChar}
+                                master={this.state.imgDir.master}
+                                delete={this.deleteLayerById}
+                            />
+                        </Aux>
                     }
-                    <hr />
-                    <ViewArticle
-                        article={this.state.activeArticle}
-                        master={this.state.imgDir.master}
-                        refresh={(id) => {
-                            this.getArticle(id);
-                            this.getCompArticles(this.state.activeComp.id);
-                        }}
-                    />
-                </Aux>
-            )
-        }
+                    {
+                        this.state.scene === 'article-browse' &&
+                        <Aux>
+                            <button onClick={() => {
+                                this.clearActiveChar();
+                                this.setState({ scene: 'comp' });
+                            }}>Back to Compendium</button>
+                            {
+                                this.state.activeChar &&
+                                <button onClick={() => { this.setState({ scene: 'char' }); }}>
+                                    Back to Character
+                            </button>
+                            }
+                            <hr />
+                            <ArticleBrowse
+                                articles={this.state.compArts}
+                                getArticle={this.getArticle}
+                                master={this.state.imgDir.master}
+                            />
+                        </Aux>
+                    }
+                    {
+                        this.state.scene === 'article' &&
+                        <Aux>
+                            <button onClick={() => {
+                                this.clearActiveChar();
+                                this.setState({ scene: 'comp' });
+                            }}>Back to Compendium</button>
+                            {
+                                this.state.activeChar &&
+                                <button onClick={() => { this.setState({ scene: 'char' }); }}>
+                                    Back to Character
+                            </button>
+                            }
+                            <hr />
+                            <ViewArticle
+                                article={this.state.activeArticle}
+                                master={this.state.imgDir.master}
+                                refresh={(id) => {
+                                    this.getArticle(id);
+                                    this.getCompArticles(this.state.activeComp.id);
+                                }}
+                            />
+                        </Aux>
+                    }
+                </div>
+            </Aux>
+        );
     };
 }
 
